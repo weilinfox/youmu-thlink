e3a8cc9adb2b160b862cba197d6bebd0eefde60d
weilinfox <weilin.king@qq.com>
2022-12-12 00:06:58 +0800

    Add broker debug module

--- broker/main.go
+++ broker/main.go
@@ -5,14 +5,23 @@ import (
 	"fmt"
 
 	broker "github.com/weilinfox/youmu-thlink/broker/lib"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 
 	listenHost := flag.String("s", "0.0.0.0:4646", "listen hostname")
 	upperHost := flag.String("u", "", "upper broker hostname")
+	debug := flag.Bool("d", false, "debug mode")
 
 	flag.Parse()
+
+	if *debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	} else {
+		logrus.SetLevel(logrus.InfoLevel)
+	}
 
 	broker.Main(*listenHost, *upperHost)
 
